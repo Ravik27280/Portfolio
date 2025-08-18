8208c7770ef739e16a5ddef585e11bd63f6cd117
Ravi Kumar Vishwakarma <47824408+Ravik27280@users.noreply.github.com>
2025-08-19 00:43:10 +0530

    Update experience.js

--- src/utils/data/experience.js
+++ src/utils/data/experience.js
@@ -1,12 +1,18 @@
 export const experiences = [
-  {
+    {
     id: 1,
-    title: "Associate Software Engineer",
+    title: "Software Engineer",
     company: "AIRBUS India",
-    duration: "(Aug 2022 - Present)",
+    duration: "(April 2025 - Present)",
   },
   {
     id: 2,
+    title: "Associate Software Engineer",
+    company: "AIRBUS India",
+    duration: "(Aug 2022 - March 2025)",
+  },
+  {
+    id: 3,
     title: "JEE with DevOps & Cloud(AWS) Intern",
     company: "Capgemini",
     duration: "(Feb 2022 - May 2022)",
